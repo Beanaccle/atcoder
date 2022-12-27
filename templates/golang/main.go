4261c8bae48ce5ec8186fe241cde6b77aa4d51ff
beanaccle <25994266+beanaccle@users.noreply.github.com>
2022-12-27 09:08:33 +0900

    update golang template

--- templates/golang/main.go
+++ templates/golang/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
 func main() {
+	defer writer.Flush()
+
 	var a, b int
-	fmt.Scan(&a, &b)
-	fmt.Println(a - b)
+	fmt.Fscan(reader, &a, &b)
+
+	fmt.Fprintln(writer, a-b)
 }
